17fee47ac45f43bd525d2c65a4027b5afe36f8e3
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2022-10-16 20:13:52 +0300

    init test spring project

---
+++ liashenkospring/src/main/java/com/example/liashenkospring/LiashenkospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.liashenkospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiashenkospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiashenkospringApplication.class, args);
+	}
+
+}
