9bf00d4426b0bf1b8bcf6fd2bfcf920d2ce5a3bc
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2022-10-17 20:57:12 +0300

    test retrofit2

--- ListViewWithBackend/app/src/main/java/com/example/listviewwithbackend/MainActivity.java
+++ ListViewWithBackend/app/src/main/java/com/example/listviewwithbackend/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.listviewwithbackend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.listviewwithbackend.pojo.Post;
+import com.example.listviewwithbackend.retorfit.NetworkService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.lblDisplayJsonPlaceHolder);
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        textView.append(post.getId() + "\n");
+                        textView.append(post.getUserId() + "\n");
+                        textView.append(post.getTitle() + "\n");
+                        textView.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+                        textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
+
     }
 }
